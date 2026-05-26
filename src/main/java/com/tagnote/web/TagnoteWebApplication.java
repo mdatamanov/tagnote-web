dfff94908f30fad226b1fa4481d0852f2b27c619
maksatamanov <maks.atamanov05@mail.ru>
2026-05-26 20:52:33 +0500

    Initial commit: setup Spring Boot project with dependencies

---
+++ src/main/java/com/tagnote/web/TagnoteWebApplication.java
@@ -0,0 +1,13 @@
+package com.tagnote.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagnoteWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagnoteWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tagnote/web/TagnoteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagnote.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagnoteWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
